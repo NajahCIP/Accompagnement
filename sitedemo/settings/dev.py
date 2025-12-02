907a52f48bf5145c597c38fecde901abe9f44110
Salma AZIZ <aziz.salma095@gmail.com>
2025-12-02 09:47:05 +0100

    Ajout de formulaire de contact fonctionnel avec un guide de configuration

--- sitedemo/settings/dev.py
+++ sitedemo/settings/dev.py
@@ -1,4 +1,8 @@
 from .base import *
+import os
+from dotenv import load_dotenv
+load_dotenv()  # charge automatiquement .env à la racine du projet
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -9,8 +13,15 @@ SECRET_KEY = "django-insecure-85ma8%6$dz!ha8(gbb0tqn3)kz@s@j7fs*k&x@**vzh^w3hd%o
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+#EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 try:
     from .local import *
